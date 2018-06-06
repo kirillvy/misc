8a8a388d00b0335004f11b4bfc78d06830e63b06
Kirill Vysokolov <kir2250@gmail.com>
2018-06-06 20:05:50 +0300

    speed test updated

--- bbb.py
+++ bbb.py
@@ -4,16 +4,16 @@ import time
 
 class RKN:
     def __init__(self, addresses):
-        all_addresses = [ IPv4Network(_) for _ in addresses ]
         self.data = set()
-        for entry in all_addresses:
-            self.data = self.data.union(entry)
+        for entry in addresses:
+            self.data = self.data.union(IPv4Network(entry))
 
     def is_banned(self, s):
         # print(self.data, s)=
         if IPv4Address(s) in self.data:
             return True
         return False
+
 
 if __name__ == "__main__":
     r = RKN(['10.0.0.0/8', '8.8.8.8/32'])
@@ -23,9 +23,9 @@ if __name__ == "__main__":
     print(r.is_banned('8.8.8.7'))  # False
     print(r.is_banned('10.255.255.255'))
 
+    x = RKN(['52.58.0.0/15', '18.196.0.0/15','18.194.0.0/15', '35.156.0.0/14'])
     #test на скорость
     t0 = time.time()
-    x = RKN(['52.58.0.0/15', '18.196.0.0/15','18.194.0.0/15', '35.156.0.0/14'])
     for y in ['52.58.0.0/15', '18.196.0.0/15','18.194.0.0/15', '35.156.0.0/14']:
         for z in list(IPv4Network(y)):
             if x.is_banned(str(z)):
@@ -36,4 +36,4 @@ if __name__ == "__main__":
     t1 = time.time()
     total = t1 - t0
     print(total)
-    # 22.4 секунды, в 3 раза быстрее, чем с массивом
+    # 9,7 секунды, в 7,5 раз быстрее, чем с массивом из 4 IPv4Network()
